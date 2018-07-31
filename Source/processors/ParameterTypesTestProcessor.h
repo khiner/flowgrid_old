312c920895988e993744dc293637d91c33c23494
Karl Hiner <karl.hiner@gmail.com>
2018-07-30 21:53:07 -0700

    Add switch parameter to types test processor

--- Source/processors/ParameterTypesTestProcessor.h
+++ Source/processors/ParameterTypesTestProcessor.h
@@ -8,18 +8,17 @@ public:
     explicit ParameterTypesTestProcessor() :
             DefaultAudioProcessor(getPluginDescription()),
             boolParameter(new AudioParameterBool("bool", "Bool", false, "val")),
-            floatParameter(new AudioParameterFloat("float", "Float", NormalisableRange<float>(0.0f, 1.0f), 0.5f, "dB",
-                                                   AudioProcessorParameter::genericParameter, defaultStringFromDbValue, nullptr)),
+            switchParameter(new AudioParameterInt("switch", "Switch", 0, 1, 1, "val")),
+
             intParameter(new AudioParameterInt("int", "Int", 0, 10, 5, "val")),
-            choiceParameter(new AudioParameterChoice("choice", "Choice", {"choice 1", "choice 2 (default)", "choice 3"}, 2, "val")) {
+            choiceParameter(new AudioParameterChoice("choice", "Choice", {"choice 1", "choice 2 (default)", "choice 3"}, 2, "val")),
+            floatParameter(new AudioParameterFloat("float", "Float", NormalisableRange<float>(0.0f, 1.0f), 0.5f, "dB",
+                                                   AudioProcessorParameter::genericParameter, defaultStringFromDbValue, nullptr)) {
         addParameter(boolParameter);
-        addParameter(floatParameter);
+        addParameter(switchParameter);
         addParameter(intParameter);
         addParameter(choiceParameter);
-    }
-
-    ~ParameterTypesTestProcessor() override {
-        boolParameter->removeListener(this);
+        addParameter(floatParameter);
     }
 
     static const String name() { return "Parameter Types Test"; }
@@ -28,17 +27,11 @@ public:
         return DefaultAudioProcessor::getPluginDescription(name(), false, false);
     }
 
-    void prepareToPlay(double sampleRate, int maximumExpectedSamplesPerBlock) override {
-    }
-
-    void parameterChanged(AudioProcessorParameter *parameter, float newValue) override {
-    }
-
-    void processBlock(AudioSampleBuffer& buffer, MidiBuffer& midiMessages) override {
-    }
+    void processBlock(AudioSampleBuffer& buffer, MidiBuffer& midiMessages) override {}
 
 private:
     AudioParameterBool *boolParameter;
+    AudioParameterInt *switchParameter;
     AudioParameterInt *intParameter;
     AudioParameterChoice *choiceParameter;
     AudioParameterFloat *floatParameter;
