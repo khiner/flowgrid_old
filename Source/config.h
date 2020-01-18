ff9dbb4055e7b0102a02058e151e94de86a31ec3
Karl Hiner <karl.hiner@gmail.com>
2020-01-18 15:12:10 -0800

    Hacky fix for libusb working so projucer builds just work without manual CMakeLists changes

---
+++ Source/config.h
@@ -0,0 +1,13 @@
+// This file is included in the libusb library as "config.h".
+// I couldn't figure out a better way for it to be found than
+// to just put it at the top-level.
+// I'm sure there are better ways to handle this!
+#if defined(__APPLE__)
+
+#include "usb/libusb/config_xcode.h"
+
+#elif defined(WIN32)
+#include "config_msvc.h"
+#else
+#include "config_linux.h"
+#endif
--- Source/usb/libusb/config.h
+++
@@ -1,9 +0,0 @@
-#if defined(__APPLE__)
-
-#include "config_xcode.h"
-
-#elif defined(WIN32)
-#include "config_msvc.h"
-#else
-#include "config_linux.h"
-#endif
