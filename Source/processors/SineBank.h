1bb3c3d8d81399fe9b225a9f352e706f0ed16ba9
Karl Hiner <karl.hiner@gmail.com>
2020-01-31 13:04:31 -0800

    Move audio_sources dir into processors dir

--- Source/processors/SineBank.h
+++ Source/processors/SineBank.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <audio_sources/ToneSourceWithParameters.h>
+#include "audio_sources/ToneSourceWithParameters.h"
 #include "JuceHeader.h"
 #include "DefaultAudioProcessor.h"
 
