9218d972d5baf370e841b9661bd7c3e8b2a5e029
Karl Hiner <karl.hiner@gmail.com>
2020-06-08 10:50:47 -0700

    Greatly simplified and improved performance of ContextPane by just rendering rectangles directly in `paint()` method, rather than using DrawableRectangles

--- Source/view/context_pane/ContextPane.h
+++ Source/view/context_pane/ContextPane.h
@@ -19,175 +19,95 @@ public:
         tracks.removeListener(this);
     }
 
-    void resized() override {
-
-        if (gridCells.isEmpty() && masterTrackGridCells.isEmpty())
-            return;
-
-        int cellWidth = 20;
-        int cellHeight = 20;
-        int newWidth = cellWidth * jmax(tracks.getNumNonMasterTracks(), view.getNumMasterProcessorSlots() + view.getGridViewTrackOffset() + 1); // + master track output
-        int newHeight = cellHeight * (view.getNumTrackProcessorSlots() + 2); // + track output row + master track
-        setSize(newWidth, newHeight);
-
-        auto r = getLocalBounds();
-
+    void paint(Graphics &g) override {
         auto trackViewOffset = view.getGridViewTrackOffset();
         auto masterViewSlotOffset = view.getMasterViewSlotOffset();
 
+        auto r = getLocalBounds();
         auto masterRowBounds = r.removeFromBottom(cellHeight);
         masterRowBounds.removeFromLeft(jmax(0, trackViewOffset - masterViewSlotOffset) * cellWidth);
 
-        auto tracksBounds = r;
-        tracksBounds.removeFromLeft(jmax(0, masterViewSlotOffset - trackViewOffset) * cellWidth);
-
-        for (auto *processorGridCell : masterTrackGridCells) {
-            processorGridCell->setRectangle(masterRowBounds.removeFromLeft(cellWidth).reduced(2).toFloat());
-        }
-
-        for (auto *trackGridCells : gridCells) {
-            auto column = tracksBounds.removeFromLeft(cellWidth);
-            for (int i = trackGridCells->size() - 1; i >= 0; i--) {
-                trackGridCells->getUnchecked(i)->setRectangle(column.removeFromBottom(cellHeight).reduced(2).toFloat());
-            }
-        }
-    }
-
-private:
-    class GridCell : public DrawableRectangle {
-    public:
-        explicit GridCell(Component *parent) {
-            setTrackAndProcessor({}, {}, true, false, false);
-            setCornerSize({3, 3});
-            parent->addAndMakeVisible(this);
-        }
-
-        void setTrackAndProcessor(const ValueTree &track, const ValueTree &processor, bool inView, bool slotSelected, bool slotFocused) {
-            const static auto &baseColour = findColour(ResizableWindow::backgroundColourId).brighter(0.4);
-
-            // this is the only part different than GraphEditorProcessorLane
-            auto colour = processor.isValid() ? findColour(TextEditor::backgroundColourId) : baseColour;
-
-            if (TracksState::doesTrackHaveSelections(track))
-                colour = colour.brighter(processor.isValid() ? 0.04 : 0.15);
-            if (slotSelected)
-                colour = TracksState::getTrackColour(track);
-            if (slotFocused)
-                colour = colour.brighter(0.16);
-            if (!inView)
-                colour = colour.darker(0.3);
-
-            setFill(colour);
-        }
-    };
-
-    TracksState &tracks;
-    ViewState &view;
-
-    // indexed by [column(track)][row(processorSlot)] rather than the usual [row][column] matrix convention.
-    // Just makes more sense in this track-first world!
-    OwnedArray<OwnedArray<GridCell> > gridCells;
-    OwnedArray<GridCell> masterTrackGridCells;
-
-    void updateGridCellColours() {
-        if (gridCells.isEmpty())
-            return;
+        auto tracksBounds = r.withTrimmedLeft(jmax(0, masterViewSlotOffset - trackViewOffset) * cellWidth);
 
         for (const auto &track : tracks.getState()) {
-            auto trackIndex = tracks.indexOf(track);
-            const auto &trackGridCells = tracks.isMasterTrack(track) ? &masterTrackGridCells : gridCells.getUnchecked(trackIndex);
+            bool isMaster = TracksState::isMasterTrack(track);
+            auto trackBounds = isMaster ? masterRowBounds : tracksBounds.removeFromLeft(cellWidth);
+            const auto &outputProcessor = TracksState::getOutputProcessorForTrack(track);
             auto trackOutputIndex = view.getSlotOffsetForTrack(track) + ViewState::getNumVisibleSlotsForTrack(track);
-            const ValueTree &outputProcessor = TracksState::getOutputProcessorForTrack(track);
-            for (auto gridCellIndex = 0; gridCellIndex < trackGridCells->size(); gridCellIndex++) {
-                auto *cell = trackGridCells->getUnchecked(gridCellIndex);
+            for (auto gridCellIndex = 0; gridCellIndex < view.getNumSlotsForTrack(track) + 1; gridCellIndex++) {
+                Colour cellColour;
                 if (gridCellIndex == trackOutputIndex) {
-                    cell->setTrackAndProcessor(track, outputProcessor, view.isTrackInView(track), true, false);
+                    cellColour = getFillColour(track, outputProcessor, view.isTrackInView(track), true, false);
                 } else {
                     int slot = gridCellIndex < trackOutputIndex ? gridCellIndex : gridCellIndex - 1;
                     const auto &processor = TracksState::getProcessorAtSlot(track, slot);
-                    cell->setTrackAndProcessor(track, processor,
+                    cellColour = getFillColour(track, processor,
                                                view.isProcessorSlotInView(track, slot),
                                                TracksState::isSlotSelected(track, slot),
                                                tracks.isSlotFocused(track, slot));
                 }
+                g.setColour(cellColour);
+                const auto &rectangle = isMaster ?
+                        trackBounds.removeFromLeft(cellWidth).reduced(2).toFloat() :
+                        trackBounds.removeFromTop(cellHeight).reduced(2).toFloat();
+                g.fillRoundedRectangle(rectangle, 3);
             }
         }
     }
 
+    void resized() override {
+        numColumns = jmax(tracks.getNumNonMasterTracks(), view.getNumMasterProcessorSlots() + view.getGridViewTrackOffset() + 1); // + master track output
+        numRows = view.getNumTrackProcessorSlots() + 2;  // + track output row + master track
+        setSize(cellWidth * numColumns, cellHeight * numRows);
+    }
+
+    Colour getFillColour(const ValueTree &track, const ValueTree &processor, bool inView, bool slotSelected, bool slotFocused) {
+        const static auto &baseColour = findColour(ResizableWindow::backgroundColourId).brighter(0.4);
+
+        // this is the only part different than GraphEditorProcessorLane
+        auto colour = processor.isValid() ? findColour(TextEditor::backgroundColourId) : baseColour;
+
+        if (TracksState::doesTrackHaveSelections(track))
+            colour = colour.brighter(processor.isValid() ? 0.04 : 0.15);
+        if (slotSelected)
+            colour = TracksState::getTrackColour(track);
+        if (slotFocused)
+            colour = colour.brighter(0.16);
+        if (!inView)
+            colour = colour.darker(0.3);
+
+        return colour;
+    }
+
+private:
+    static constexpr int cellWidth = 20, cellHeight = 20;
+
+    int numRows, numColumns;
+    TracksState &tracks;
+    ViewState &view;
+
     void valueTreeChildAdded(ValueTree &parent, ValueTree &child) override {
-        if (child.hasType(IDs::TRACK)) {
-            if (TracksState::isMasterTrack(child)) {
-                return valueTreePropertyChanged(view.getState(), IDs::numMasterProcessorSlots);
-            } else {
-                auto *newGridCellColumn = new OwnedArray<GridCell>();
-                int numProcessorSlots = view.getNumTrackProcessorSlots() + 1; // + track output
-                for (int processorSlot = 0; processorSlot < numProcessorSlots; processorSlot++) {
-                    newGridCellColumn->add(new GridCell(this));
-                }
-                gridCells.add(newGridCellColumn);
-            }
+        if (child.hasType(IDs::TRACK))
             resized();
-        } else if (child.hasType(IDs::PROCESSOR)) {
-            updateGridCellColours();
-        }
+        else if (child.hasType(IDs::PROCESSOR))
+            repaint();
     }
 
     void valueTreeChildRemoved(ValueTree &exParent, ValueTree &child, int index) override {
-        if (child.hasType(IDs::TRACK)) {
-            if (TracksState::isMasterTrack(child)) {
-                masterTrackGridCells.clear();
-            } else {
-                gridCells.remove(index);
-            }
+        if (child.hasType(IDs::TRACK))
             resized();
-            updateGridCellColours();
-        } else if (child.hasType(IDs::PROCESSOR)) {
-            updateGridCellColours();
-        }
+        else if (child.hasType(IDs::PROCESSOR))
+            repaint();
     }
 
     void valueTreePropertyChanged(ValueTree &tree, const Identifier &i) override {
         if (tree.hasType(IDs::VIEW_STATE)) {
-            if (i == IDs::numProcessorSlots) {
-                if (gridCells.isEmpty())
-                    return;
-                int numProcessorSlots = int(tree[IDs::numProcessorSlots]) + 1; // + track output
-                while (gridCells.getFirst()->size() < numProcessorSlots) {
-                    for (auto trackIndex = 0; trackIndex < gridCells.size(); trackIndex++) {
-                        auto *trackGridCells = gridCells.getUnchecked(trackIndex);
-                        trackGridCells->add(new GridCell(this));
-                    }
-                }
-                while (gridCells.getFirst()->size() > numProcessorSlots) {
-                    for (auto trackIndex = 0; trackIndex < gridCells.size(); trackIndex++) {
-                        auto *trackGridCells = gridCells.getUnchecked(trackIndex);
-                        trackGridCells->removeLast(1, true);
-                    }
-                }
+            if (i == IDs::numProcessorSlots || i == IDs::numMasterProcessorSlots || i == IDs::gridViewTrackOffset || i == IDs::masterViewSlotOffset)
                 resized();
-                updateGridCellColours();
-            } else if (i == IDs::numMasterProcessorSlots) {
-                int numProcessorSlots = int(tree[IDs::numMasterProcessorSlots]) + 1; // + track output
-                while (masterTrackGridCells.size() < numProcessorSlots) {
-                    masterTrackGridCells.add(new GridCell(this));
-                }
-                masterTrackGridCells.removeLast(masterTrackGridCells.size() - numProcessorSlots, true);
-                resized();
-                updateGridCellColours();
-            } else if (i == IDs::gridViewTrackOffset || i == IDs::masterViewSlotOffset) {
-                resized();
-                updateGridCellColours();
-            } else if (i == IDs::focusedTrackIndex || i == IDs::focusedProcessorSlot || i == IDs::gridViewSlotOffset) {
-                updateGridCellColours();
-            }
+            else if (i == IDs::focusedTrackIndex || i == IDs::focusedProcessorSlot || i == IDs::gridViewSlotOffset)
+                repaint();
         } else if (i == IDs::selectedSlotsMask) {
-            updateGridCellColours();
+            repaint();
         }
     }
-
-    void valueTreeChildOrderChanged(ValueTree &tree, int, int) override {}
-
-    void valueTreeParentChanged(ValueTree &) override {}
-
-    void valueTreeRedirected(ValueTree &) override {}
 };
