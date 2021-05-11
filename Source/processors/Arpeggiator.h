b159101f4d08055f84e313473932f774c561e976
Karl Hiner <karl.hiner@gmail.com>
2021-05-10 20:07:47 -0700

    Remove use of deprecated `MidiBuffer::Iterator`

--- Source/processors/Arpeggiator.h
+++ Source/processors/Arpeggiator.h
@@ -4,7 +4,6 @@
 
 class Arpeggiator : public DefaultAudioProcessor {
 public:
-
     Arpeggiator() : DefaultAudioProcessor(getPluginDescription(), AudioChannelSet::disabled()) {
         addParameter(speed = new AudioParameterFloat("speed", "Arpeggiator Speed", 0.0, 1.0, 0.5));
     }
@@ -34,13 +33,11 @@ public:
     }
 
     void processBlock(AudioBuffer<float> &buffer, MidiBuffer &midi) override {
-        // however we use the buffer to get timing information
         auto numSamples = buffer.getNumSamples();
         auto noteDuration = static_cast<int> (std::ceil(rate * 0.25f * (0.1f + (1.0f - (*speed)))));
 
-        MidiMessage msg;
-        int ignore;
-        for (MidiBuffer::Iterator it(midi); it.getNextEvent(msg, ignore);) {
+        for (const auto metadata : midi) {
+            const auto msg = metadata.getMessage();
             if (msg.isNoteOn()) notes.add(msg.getNoteNumber());
             else if (msg.isNoteOff()) notes.removeValue(msg.getNoteNumber());
         }
@@ -49,18 +46,15 @@ public:
 
         if ((time + numSamples) >= noteDuration) {
             auto offset = jmax(0, jmin((int) (noteDuration - time), numSamples - 1));
-
             if (lastNoteValue > 0) {
                 midi.addEvent(MidiMessage::noteOff(1, lastNoteValue), offset);
                 lastNoteValue = -1;
             }
-
             if (notes.size() > 0) {
                 currentNote = (currentNote + 1) % notes.size();
                 lastNoteValue = notes[currentNote];
                 midi.addEvent(MidiMessage::noteOn(1, lastNoteValue, (uint8) 127), offset);
             }
-
         }
 
         time = (time + numSamples) % noteDuration;
