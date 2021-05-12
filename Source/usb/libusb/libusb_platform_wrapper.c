df15bd380ea9de2b56457f34107a83f18da350b6
Karl Hiner <karl.hiner@gmail.com>
2021-05-11 18:39:31 -0700

    Update libusb

--- Source/usb/libusb/libusb_platform_wrapper.c
+++ Source/usb/libusb/libusb_platform_wrapper.c
@@ -1,15 +1,17 @@
 #if defined(__APPLE__)
 
-# include "core.c"
-# include "descriptor.c"
-# include "hotplug.c"
-# include "io.c"
-# include "strerror.c"
-# include "sync.c"
+#define PLATFORM_POSIX
 
-# include "os/darwin_usb.c"
-# include "os/poll_posix.c"
-# include "os/threads_posix.c"
+#include "core.c"
+#include "descriptor.c"
+#include "hotplug.c"
+#include "io.c"
+#include "strerror.c"
+#include "sync.c"
+
+#include "os/darwin_usb.c"
+#include "os/events_posix.c"
+#include "os/threads_posix.c"
 
 #else
 // On linux, we link directly to the system library installed through
